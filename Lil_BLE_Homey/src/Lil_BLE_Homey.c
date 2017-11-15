1381d878d81acb3e4d407d0b5c90f5e4408d48d4
robertkl <liujiayi@umich.edu>
2017-11-15 17:58:22 -0500

    Added Lil BLE Homey

---
+++ Lil_BLE_Homey/src/Lil_BLE_Homey.c
@@ -0,0 +1,136 @@
+/******************** (C) COPYRIGHT 2015 STMicroelectronics ********************
+* File Name          : Lil_BLE_Homey.c
+* Author             : AMS - VMA RF Application team
+* Version            : V1.0.0
+* Date               : 1-December-2015
+* Description        : BlueNRG-1 main file for Chat Master & Slave demo
+********************************************************************************
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*******************************************************************************/
+/** @addtogroup BlueNRG1_demonstrations_applications
+ * BlueNRG-1 Chat Master and Slave demo \see BLE_Chat_Master_Slave_main.c for documentation.
+ *
+ *@{
+ */
+
+/** @} */
+/** \cond DOXYGEN_SHOULD_SKIP_THIS
+ */
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include <string.h>
+#include "BlueNRG1_conf.h"
+#include "ble_const.h"
+#include "bluenrg1_stack.h"
+#include "chat.h"
+#include "clock.h"
+#include "SDK_EVAL_Config.h"
+#include "Chat_config.h"
+
+/* External variables --------------------------------------------------------*/
+/* Private typedef -----------------------------------------------------------*/
+/* Private defines -----------------------------------------------------------*/
+#define LIL_BLE_HOMEY_VERSION_STRING "0.0.1"
+
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+/* Private macros ------------------------------------------------------------*/
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
+
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/*******************************************************************************
+* Function Name  : main.
+* Description    : Main routine.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int main(void)
+{
+  uint8_t ret;
+
+  /* System Init */
+  SystemInit();
+
+  /* Identify BlueNRG1 platform */
+  SdkEvalIdentification();
+  
+  /* Init Clock */
+  Clock_Init();
+
+  /* Configure I/O communication channel:
+       It requires the void IO_Receive_Data(uint8_t * rx_data, uint16_t data_size) function
+       where user received data should be processed */
+  SdkEvalComIOConfig(Process_InputData);
+
+  /* BlueNRG-1 stack init */
+  ret = BlueNRG_Stack_Initialization(&BlueNRG_Stack_Init_params);
+  if (ret != BLE_STATUS_SUCCESS) {
+    printf("Error in BlueNRG_Stack_Initialization() 0x%02x\r\n", ret);
+    while(1);
+  }
+  
+  printf("BlueNRG-2 Lil Homey Application (version: %s)\r\n", LIL_BLE_HOMEY_VERSION_STRING);
+  PRINTF("[DEBUG] \n");
+
+  /* Init Chat Device */
+  ret = Lil_BLE_HomeyInit();
+  if (ret != BLE_STATUS_SUCCESS) {
+    PRINTF("Lil_BLE_HomeyInit()--> Failed 0x%02x\r\n", ret);
+    while(1);
+  }
+  
+  PRINTF("BLE Stack Initialized & Device Configured\r\n");
+
+  while(1) {
+    /* Disable UART IRQ to avoid calling BLE stack functions while BTLE_StackTick() is running. */
+    NVIC_DisableIRQ(UART_IRQn);
+    
+    /* BlueNRG-1 stack tick */
+    BTLE_StackTick();
+    NVIC_EnableIRQ(UART_IRQn);
+
+    /* Application tick */
+    APP_Tick();
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+/*******************************************************************************
+* Function Name  : assert_failed
+* Description    : Reports the name of the source file and the source line number
+*                  where the assert_param error has occurred.
+* Input          : - file: pointer to the source file name
+*                  - line: assert_param error line source number
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void assert_failed(uint8_t* file, uint32_t line)
+{
+    /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    
+    /* Infinite loop */
+    while (1)
+    {}
+}
+#endif
+
+/******************* (C) COPYRIGHT 2015 STMicroelectronics *****END OF FILE****/
+/** \endcond
+ */
---
+++ Lil_BLE_Homey/src/gatt_db.c
@@ -0,0 +1,150 @@
+
+#include <stdio.h>
+#include <string.h>
+#include "ble_const.h" 
+#include "bluenrg1_stack.h"
+#include "app_state.h"
+#include "osal.h"
+#include "SDK_EVAL_Config.h"
+
+//#if DEBUG
+#define PRINTF(...) printf(__VA_ARGS__)
+//#else
+//#define PRINTF(...)
+//#endif
+
+#define COPY_UUID_128(uuid_struct, uuid_15, uuid_14, uuid_13, uuid_12, uuid_11, uuid_10, uuid_9, uuid_8, uuid_7, uuid_6, uuid_5, uuid_4, uuid_3, uuid_2, uuid_1, uuid_0) \
+  do {\
+  	uuid_struct.uuid128[0] = uuid_0; uuid_struct.uuid128[1] = uuid_1; uuid_struct.uuid128[2] = uuid_2; uuid_struct.uuid128[3] = uuid_3; \
+	uuid_struct.uuid128[4] = uuid_4; uuid_struct.uuid128[5] = uuid_5; uuid_struct.uuid128[6] = uuid_6; uuid_struct.uuid128[7] = uuid_7; \
+	uuid_struct.uuid128[8] = uuid_8; uuid_struct.uuid128[9] = uuid_9; uuid_struct.uuid128[10] = uuid_10; uuid_struct.uuid128[11] = uuid_11; \
+	uuid_struct.uuid128[12] = uuid_12; uuid_struct.uuid128[13] = uuid_13; uuid_struct.uuid128[14] = uuid_14; uuid_struct.uuid128[15] = uuid_15; \
+	}while(0)
+
+uint16_t ServHandle,
+	TemperatureCharHandle,
+	HumidityCharHandle,
+	MotionDetectedCharHandle,
+	BlindCurrPosCharHandle,
+	BlindTargPosCharHandle,
+	BlindPosStateCharHandle;
+
+/* UUIDs */
+Service_UUID_t service_uuid;
+Char_UUID_t char_uuid;
+
+/*******************************************************************************
+* Function Name  : Add_ChatService
+* Description    : Add the 'Accelerometer' service.
+* Input          : None
+* Return         : Status.
+*******************************************************************************/
+uint8_t Add_ChatService(void)
+{
+  uint8_t ret;
+
+  /*
+  UUIDs:
+  Service  			FF1738E0-B19E-11E2-9E96-0800200C9A66
+
+  Temperature  		001738F1-B19E-11E2-9E96-0800200C9A66
+  Humidity 			001738F2-B19E-11E2-9E96-0800200C9A66
+  MotionDetected 	001738F2-B19E-11E2-9E96-0800200C9A66
+
+  BlindCurrPos 		111738B1-B19E-11E2-9E96-0800200C9A66
+  BlindTargPos 		111738B2-B19E-11E2-9E96-0800200C9A66
+  BlindPosState 	111738B2-B19E-11E2-9E96-0800200C9A66
+  */
+  const uint8_t uuid[16] = 						{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+		  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+												  0xe0, 0x38, 0x17, 0xFF };
+  const uint8_t charUuidTemperature[16] = 		{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+		  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+												  0xf1, 0x38, 0x17, 0x00 };
+  const uint8_t charUuidHumidity[16] = 			{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+	  	  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+												  0xf2, 0x38, 0x17, 0x00 };
+  const uint8_t charUuidMotionDetected[16] = 	{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+  	  	  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+												  0xf3, 0x38, 0x17, 0x00 };
+  const uint8_t charUuidBlindCurrPos[16] = 		{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+	  	  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+						  	  	  	  	  	  	  0xb1, 0x38, 0x17, 0x11 };
+  const uint8_t charUuidBlindTargPos[16] = 		{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+	  	  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+												  0xb2, 0x38, 0x17, 0x11 };
+  const uint8_t charUuidBlindPosState[16] = 	{ 0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08,
+  	  	  	  	  	  	  	  	  	  	  	  	  0x96, 0x9e, 0xe2, 0x11, 0x9e, 0xb1,
+												  0xb3, 0x38, 0x17, 0x11 };
+
+  Osal_MemCpy(&service_uuid.Service_UUID_128, uuid, 16);
+  ret = aci_gatt_add_service(UUID_TYPE_128, &service_uuid, PRIMARY_SERVICE, 17 /* numAttributeRecords */, &ServHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;    
+
+  Osal_MemCpy(&char_uuid.Char_UUID_128, charUuidBlindCurrPos, 16);
+  ret =  aci_gatt_add_char(ServHandle, UUID_TYPE_128, &char_uuid, 20, CHAR_PROP_WRITE|CHAR_PROP_WRITE_WITHOUT_RESP, ATTR_PERMISSION_NONE, GATT_NOTIFY_ATTRIBUTE_WRITE,
+                16, 1, &BlindCurrPosCharHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;
+  PRINTF("[Added Char] BlindCurrPos UUID[12] = %02X\n", charUuidBlindCurrPos[12]);
+
+  Osal_MemCpy(&char_uuid.Char_UUID_128, charUuidHumidity, 16);
+  ret =  aci_gatt_add_char(ServHandle, UUID_TYPE_128, &char_uuid, 4, CHAR_PROP_NOTIFY, ATTR_PERMISSION_NONE, 0,
+                16, 0, &HumidityCharHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;
+  PRINTF("[Added Char] Humidity UUID[12] = %02X\n", charUuidHumidity[12]);
+
+  Osal_MemCpy(&char_uuid.Char_UUID_128, charUuidMotionDetected, 16);
+  ret =  aci_gatt_add_char(ServHandle, UUID_TYPE_128, &char_uuid, 1, CHAR_PROP_NOTIFY, ATTR_PERMISSION_NONE, 0,
+				16, 0, &MotionDetectedCharHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;
+  PRINTF("[Added Char] MotionDetected UUID[12] = %02X\n", charUuidMotionDetected[12]);
+
+  Osal_MemCpy(&char_uuid.Char_UUID_128, charUuidTemperature, 16);
+  ret =  aci_gatt_add_char(ServHandle, UUID_TYPE_128, &char_uuid, 4, CHAR_PROP_NOTIFY, ATTR_PERMISSION_NONE, 0,
+                  16, 0, &TemperatureCharHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;
+  PRINTF("[Added Char] Temperature UUID[12] = %02X\n", charUuidTemperature[12]);
+
+  Osal_MemCpy(&char_uuid.Char_UUID_128, charUuidBlindTargPos, 16);
+  ret =  aci_gatt_add_char(ServHandle, UUID_TYPE_128, &char_uuid, 20, CHAR_PROP_WRITE|CHAR_PROP_WRITE_WITHOUT_RESP, ATTR_PERMISSION_NONE, GATT_NOTIFY_ATTRIBUTE_WRITE,
+                16, 1, &BlindTargPosCharHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;
+  PRINTF("[Added Char] BlindTargPos UUID[12] = %02X\n", charUuidBlindTargPos[12]);
+
+  Osal_MemCpy(&char_uuid.Char_UUID_128, charUuidBlindPosState, 16);
+  ret =  aci_gatt_add_char(ServHandle, UUID_TYPE_128, &char_uuid, 20, CHAR_PROP_WRITE|CHAR_PROP_WRITE_WITHOUT_RESP, ATTR_PERMISSION_NONE, GATT_NOTIFY_ATTRIBUTE_WRITE,
+                16, 1, &BlindPosStateCharHandle);
+  if (ret != BLE_STATUS_SUCCESS) goto fail;
+  PRINTF("[Added Char] BlindPosState UUID[12] = %02X\n", charUuidBlindPosState[12]);
+
+  printf("[Added Service]\n\n");
+  return BLE_STATUS_SUCCESS; 
+
+fail:
+  printf("Error while adding Chat service.\n");
+  return BLE_STATUS_ERROR ;
+}
+
+/*******************************************************************************
+* Function Name  : Attribute_Modified_CB
+* Description    : Attribute modified callback.
+* Input          : Attribute handle modified.
+*                  Length of the data.
+*                  Attribute data.
+* Return         : None.
+*******************************************************************************/
+void Attribute_Modified_CB(uint16_t handle, uint16_t data_length, uint8_t *att_data)
+{
+  if(handle == BlindCurrPosCharHandle + 1)
+  {
+    for(int i = 0; i < data_length; i++)
+      printf("%c", att_data[i]);
+  }
+  else if(handle == TemperatureCharHandle + 2)
+  {        
+    if(att_data[0] == 0x01)
+      APP_FLAG_SET(NOTIFICATIONS_ENABLED);
+  }
+}
+
+
